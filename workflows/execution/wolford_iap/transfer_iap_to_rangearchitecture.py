dc7b694eeb6b2f0b98643dd5276f503c1a2af203
Florian Gamper <florian.gamper@datacrag.com>
2018-08-01 20:49:39 +0800

    correct import error

--- workflows/execution/wolford_iap/transfer_iap_to_rangearchitecture.py
+++ workflows/execution/wolford_iap/transfer_iap_to_rangearchitecture.py
@@ -1,13 +1,9 @@
 import sys
 import os
 
-from sqlalchemy.ext.automap import automap_base
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(sys.path[0]))))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.projects.settings_wolford_iap.project")
 
-import django
-django.setup()
+from sqlalchemy.ext.automap import automap_base
 
 from workflows.execution.wolford_iap import Session, engine
 from workflows.transformations.projects.wolford_iap.upsert_rangearchitecture import upsert_rangearchitecture
